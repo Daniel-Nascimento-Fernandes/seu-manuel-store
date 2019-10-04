fb65461c339e07bcff1c63027a990844d3986976
Daniel Fernandes <dani.fernandes9853@gmail.com>
2019-10-04 11:54:16 +0100

    ProductController Done

--- src/main/java/com/seumanuel/store/controller/ProductController.java
+++ src/main/java/com/seumanuel/store/controller/ProductController.java
@@ -1,4 +1,49 @@
 package com.seumanuel.store.controller;
 
+import com.seumanuel.store.model.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product){
+        return service.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable String id, @RequestBody Product product){
+        return service.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+        service.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable String id){
+            return service.findById(id);
+    }
+
+    @GetMapping
+    public List<Product> findByName(@RequestParam String name){
+        return service.findByName(name);
+    }
+
+    GetMapping
+    public List<Product> findByPrice(@RequestParam Integer min, @RequestParam Integer max){
+        return service.findByPrice(min,max);
+    }
+
 }
